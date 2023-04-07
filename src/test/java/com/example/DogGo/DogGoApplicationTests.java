03e975d1c29855b828b0e36e890a70f04f48e129
Furkan <furkanceylan9834@gmail.com>
2023-04-07 18:07:31 +0300

    DogGı
    Walk with me

---
+++ src/test/java/com/example/DogGo/DogGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DogGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
